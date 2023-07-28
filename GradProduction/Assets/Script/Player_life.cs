d8e9f35272179f23e67149ed7e55634d246cb08d
ootamoriki1105 <moriki-oota-20h@stu.kbc.ac.jp>
2023-07-28 09:54:12 +0900

    ライフ処理_多分できた

--- GradProduction/Assets/Script/Player_life.cs
+++ GradProduction/Assets/Script/Player_life.cs
@@ -1,19 +1,24 @@
 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_life : MonoBehaviour
 {
-    private int count = 10;     //ライフの数
+    private int count = 10; // ライフの数
 
-    void OnCollisionEnter(Collision collision)　//オブジェクトにヒットしたら
+    void OnCollisionEnter(Collision other)
     {
+        // ボールにぶつかったとき
+        if (other.gameObject.tag == "Enemy")
+        {
+            --count; // ライフを1減らす
+            Debug.Log(count);
+        }
+        else
+        {
 
-            for (int i = count; i >= 1; --i)  //10から1づつ引いていきたい
-            {
-                Debug.Log(i);
-
-            }
+        }
     }
 }
