ebcf36955aa0a785f5dbc8d08b7d410949a0dd30
ootamoriki1105 <moriki-oota-20h@stu.kbc.ac.jp>
2023-11-13 11:39:48 +0900

    ボタンを押したらオブジェクトを出せるようにした

--- GradProduction/Assets/Script/Button_Script.cs
+++ GradProduction/Assets/Script/Button_Script.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject towerPrefab; // TowerTestのプレハブ
+    private bool towerSpawned = false;
+
     void Start()
     {
-        
+        // ボタンにクリック時の処理を追加
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    // ボタンがクリックされたときに呼び出される関数
+    private void OnButtonClick()
     {
-        
+        // まだタワーがスポーンされていない場合
+        if (!towerSpawned)
+        {
+            SpawnTowerAtPosition(new Vector3(-8.25f, 4f, 61.3f));
+            towerSpawned = true; // スポーン済みフラグを設定
+        }
+    }
+
+    // 指定された座標にタワーをスポーンする関数
+    private void SpawnTowerAtPosition(Vector3 position)
+    {
+        Instantiate(towerPrefab, position, Quaternion.identity);
     }
 }
